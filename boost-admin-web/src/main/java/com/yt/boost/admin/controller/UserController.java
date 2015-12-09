d105e5eec3895f5f5213a9313550880eb369479c
tim <707445391@qq.com>
2015-12-09 18:44:51 +0800

    dev

--- boost-admin-web/src/main/java/com/yt/boost/admin/controller/UserController.java
+++ boost-admin-web/src/main/java/com/yt/boost/admin/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.yt.boost.admin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by tim on 2015/12/8.
@@ -8,4 +11,9 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class UserController {
 
+    @RequestMapping(value = "index",method = RequestMethod.GET)
+    @ResponseBody
+    public String test(){
+        return "a";
+    }
 }
